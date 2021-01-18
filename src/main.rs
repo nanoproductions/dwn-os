c0993ae878daef91db79a03feed51349713d0186
Nano Adam <nanoproductions2@gmail.com>
2021-01-18 17:50:24 -0500

    Comment out GUI

--- src/main.rs
+++ src/main.rs
@@ -27,17 +27,17 @@ pub extern "C" fn _start() -> ! {
 	// invoke breakpoint exception
 	//  x86_64::instructions::interrupts::int3();
 
-	let mode = Graphics640x480x16::new();
-	mode.set_mode();
-	mode.clear_screen(Color16::Blue);
-	mode.draw_line((80, 60), (80, 420), Color16::White);
-	mode.draw_line((80, 60), (540, 60), Color16::White);
-	mode.draw_line((80, 420), (540, 420), Color16::White);
-	mode.draw_line((540, 420), (540, 60), Color16::White);
-	mode.draw_line((80, 90), (540, 90), Color16::White);
-	for(offset, character) in "Hello World".chars().enumerate() {
-		mode.draw_character(280 + offset * 8, 72, character, Color16::White);
-	}
+	// let mode = Graphics640x480x16::new();
+	// mode.set_mode();
+	// mode.clear_screen(Color16::Blue);
+	// mode.draw_line((80, 60), (80, 420), Color16::White);
+	// mode.draw_line((80, 60), (540, 60), Color16::White);
+	// mode.draw_line((80, 420), (540, 420), Color16::White);
+	// mode.draw_line((540, 420), (540, 60), Color16::White);
+	// mode.draw_line((80, 90), (540, 90), Color16::White);
+	// for(offset, character) in "Hello World".chars().enumerate() {
+	// 	mode.draw_character(280 + offset * 8, 72, character, Color16::White);
+	// }
 
 	#[cfg(test)]
 	test_main();
